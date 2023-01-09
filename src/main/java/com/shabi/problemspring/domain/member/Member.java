156e7351f03df054adbd5661f462aea601210153
PersesTitan <torampersesTitan@gmail.com>
2023-01-09 14:53:54 +0900

    add update, create logic

--- src/main/java/com/shabi/problemspring/domain/member/Member.java
+++ src/main/java/com/shabi/problemspring/domain/member/Member.java
@@ -1,12 +1,10 @@
 package com.shabi.problemspring.domain.member;
 
+import com.shabi.problemspring.domain.member.dto.MemberDTO;
 import com.shabi.problemspring.domain.problem.Problem;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.PastOrPresent;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.cglib.core.Local;
 
 import java.io.Serializable;
@@ -21,32 +19,45 @@ import java.util.List;
 public class Member implements Serializable {
     @Id
     @GeneratedValue
-    @Column(unique = true, name = "member_id")
+    @Column(name = "member_id")
     private Long id;
 
     @Column(unique = true)
     private String loginId;
     private String password;
 
+    private final LocalDateTime createDate = LocalDateTime.now(); // 생성일
+
     @PastOrPresent
-    private LocalDateTime createDate;
-    @PastOrPresent
-    private LocalDateTime updateDate;
+    private LocalDateTime updateDate;       // 최종 수정일
 
     @ToString.Exclude
     @JoinColumn(name = "problem_id")
-    @OneToMany(mappedBy = "member", orphanRemoval = true)
+    @OneToMany(targetEntity = Problem.class, cascade = CascadeType.ALL)
     private final List<Problem> problems = new ArrayList<>();
 
-    private Member(String loginId, String password, LocalDateTime createDate, LocalDateTime updateDate) {
+    private Member(String loginId, String password) {
         this.loginId = loginId;
         this.password = password;
-        this.createDate = createDate;
-        this.updateDate = updateDate;
+        this.updateDate = LocalDateTime.now();
     }
 
-    public void addProblem(Problem problem) {
-        this.problems.add(problem);
+    public static Member create(String loginId, String password) {
+        return new Member(loginId, password);
     }
 
+    public static Member create(MemberDTO memberDTO) {
+        return new Member(memberDTO.loginId(), memberDTO.password());
+    }
+
+    // 업데이트 하였을때 현재 시간으로 수정하는 로직
+    public void update() {
+        this.updateDate = LocalDateTime.now();
+    }
+
+    public void update(String loginId, String password) {
+        update();
+        this.loginId = loginId;
+        this.password = password;
+    }
 }
