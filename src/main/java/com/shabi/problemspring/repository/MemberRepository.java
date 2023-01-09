f32bfdee604965674f590f68af43fb8ea9444c09
PersesTitan <torampersesTitan@gmail.com>
2023-01-09 14:54:25 +0900

    add save, remove, findOne, findAll

--- src/main/java/com/shabi/problemspring/repository/MemberRepository.java
+++ src/main/java/com/shabi/problemspring/repository/MemberRepository.java
@@ -1,7 +1,31 @@
 package com.shabi.problemspring.repository;
 
+import com.shabi.problemspring.domain.member.Member;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public void remove(Member member) {
+        em.remove(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("SELECT M FROM Member AS M", Member.class)
+                .getResultList();
+    }
 }
