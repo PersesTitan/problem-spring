2e9c668acbba5b127606a73f9061e1edbba3f69d
PersesTitan <torampersesTitan@gmail.com>
2023-01-10 00:42:00 +0900

    create MainDAO

---
+++ src/main/java/com/shabi/problemspring/controller/dao/MainDAO.java
@@ -0,0 +1,8 @@
+package com.shabi.problemspring.controller.dao;
+
+import com.shabi.problemspring.domain.problem.Problem;
+
+import java.util.List;
+
+public record MainDAO(List<Problem> problems) {
+}
